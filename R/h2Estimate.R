4161c2c899411160f761ee5157fb3b2c3cdaab06
scarpino <scarpino@utexas.edu>
2019-08-16 15:05:04 -0400

    updating a bug in model comparisons where the deviance is <= 0

--- R/h2Estimate.R
+++ R/h2Estimate.R
@@ -68,28 +68,33 @@ h2Estimate <- function(data, nreps = 1000) {
     hm1.real = glmer(cbind(trait1, trait2) ~ (1 | dad), data = data, 
         family = binomial)
     Dobs = as.numeric(deviance(hm0.real) - deviance(hm1.real))
-    
-    ncases <- nrow(data)
-    perm1 = replicate(nreps, {
+    if(Dobs < 1e-5){
+      pval = 1
+      deviance = Dobs
+      sim = NA 
+      trueTau2 <- 0
+      h2 <- 0
+    }else{
+      ncases <- nrow(data)
+      perm1 = replicate(nreps, {
         neworder = sample(1:ncases, ncases)
         ptable = data.frame(dad = data$dad, t1 = data$trait1[neworder], 
-            t2 = data$trait2[neworder])
+                            t2 = data$trait2[neworder])
         hm0 = glm(cbind(t1, t2) ~ 1, data = ptable, family = binomial)
         # hm1 = glmer(cbind(t1,t2) ~ (1 | dad), data=ptable,
         # family=binomial, REML=FALSE)
         hm1 = glmer(cbind(t1, t2) ~ (1 | dad), data = ptable, 
-            family = binomial)
+                    family = binomial)
         D = as.numeric(deviance(hm0) - deviance(hm1))
         D
-    })
-    
-    pval <- length(which(perm1 > Dobs))/length(perm1)
-    
-    trueTau2 <- (VarCorr(hm1.real)$dad[1])^2
-    h2 <- 4 * (trueTau2/(trueTau2 + (pi/sqrt(3))^2))
+      })
+      pval <- length(which(perm1 > Dobs))/length(perm1)
+      trueTau2 <- (VarCorr(hm1.real)$dad[1])^2
+      h2 <- 4 * (trueTau2/(trueTau2 + (pi/sqrt(3))^2))
+    }
     
     out <- list(h2 = h2, pval = pval, deviance = Dobs, sim = perm1, 
-        obsMod = hm1.real)
+                trueTau2 = trueTau2, obsMod_glmer = hm1.real, obsMod_glm = hm0.real)
     
     return(out)
 }

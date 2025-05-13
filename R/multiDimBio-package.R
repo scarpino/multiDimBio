6addfa4af463b0d89a3a71ecbe392be19c8e16f4
Sam <sscarpin@gmail.com>
2025-05-13 11:06:10 -0400

    updating author field for cran submission

--- R/multiDimBio-package.R
+++ R/multiDimBio-package.R
@@ -119,7 +119,7 @@ NULL
 #' methodological and graphical.
 #' 
 #' \tabular{ll}{ Package: \tab multiDimBio\cr Type: \tab Package\cr Version:
-#' \tab 1.2.3\cr Date: \tab 2025-05-13\cr License: \tab GPL 3.0\cr LazyLoad:
+#' \tab 1.2.4\cr Date: \tab 2025-05-13\cr License: \tab GPL 3.0\cr LazyLoad:
 #' \tab yes\cr } The datasets are: Nuclei, Groups, CondA, CondB, Scores, and
 #' Dyad
 #' 
